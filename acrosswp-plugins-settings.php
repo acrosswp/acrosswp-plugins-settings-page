c4ce038278e53819bfd4e4a0894389e2c7a6bd99
raftaar1191 <raftaar1191@gmail.com>
2023-11-12 21:26:56 +0530

    update code

--- acrosswp-plugins-settings.php
+++ acrosswp-plugins-settings.php
@@ -25,21 +25,12 @@ if( ! class_exists( 'AcrossWP_Sub_Menu' ) ) {
 	abstract class AcrossWP_Sub_Menu {
 
 		/**
-		 * The single instance of the class.
-		 *
-		 * @var AcrossWP_Sub_Menu
-		 * @since 0.0.1
-		 */
-		protected static $_instance = null;
-
-		/**
 		 * The single instance of the class AcrossWP_Main_Menu.
 		 *
 		 * @var AcrossWP_Main_Menu
 		 * @since 0.0.1
 		 */
 		public $main_menu;
-
 
 		/**
 		 * Initialize the collections used to maintain the actions and filters.
@@ -48,35 +39,10 @@ if( ! class_exists( 'AcrossWP_Sub_Menu' ) ) {
 		 */
 		public function __construct() {
 			$this->main_menu = AcrossWP_Main_Menu::instance();
-		}
 
-		/**
-		 * All the hooks will be here
-		 */
-		public function hooks () {
-
-			/**
-			 * Add the parent menu into the Admin Dashboard
-			 */
 			add_action( 'admin_menu', array( $this, 'menu' ), 1000 );
 		}
 
-		/**
-		 * Main Post_Anonymously_Loader Instance.
-		 *
-		 * Ensures only one instance of WooCommerce is loaded or can be loaded.
-		 *
-		 * @since 0.0.1
-		 * @static
-		 * @see Post_Anonymously_Loader()
-		 * @return Post_Anonymously_Loader - Main instance.
-		 */
-		public static function instance() {
-			if ( is_null( self::$_instance ) ) {
-				self::$_instance = new self();
-			}
-			return self::$_instance;
-		}
 
 		/**
 		 * Define constant if not already set
